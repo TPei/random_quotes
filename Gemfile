d85dbe944d7f396756384f1c5ffed3a074816825
tpei <t.s.peikert@gmail.com>
2016-12-07 07:22:00 +0100

    add specs for service

--- Gemfile
+++ Gemfile
@@ -1,8 +1,13 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'sequel'
 gem 'rack'
 gem 'rake'
 gem 'puma'
 gem 'dotenv'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+  gem 'rspec-instafail', require: false
+end
---
+++ spec/main_controller_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+require 'json'
+require './random_controller.rb'
+
+RSpec.describe RandomController do
+  def app
+    RandomController
+  end
+
+  describe 'GET /info' do
+    it 'returns HELLO API-WORLD' do
+      get '/info'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'Hello! / returns random quote from a quote_file. /:id returns a specific quote from file'
+    end
+  end
+
+  describe 'GET /' do
+    before do
+      ENV['FILEPATH'] = './quotes/example_quotes.json'
+    end
+
+    it 'returns a random json quote from file' do
+      get '/'
+      expect(last_response.status).to eq 200
+      response = last_response.body
+      quote = JSON.parse(response)
+      expect(quote.class).to eq Hash
+      expect(quote.has_key?('who')).to eq true
+      expect(quote.has_key?('what')).to eq true
+      expect(quote.has_key?('where')).to eq true
+      expect(quote.has_key?('when')).to eq true
+      expect(quote.has_key?('id')).to eq true
+      expect(quote.has_key?('permalink')).to eq true
+      expect(quote['permalink'].include?(quote['id'])).to eq true
+    end
+  end
+
+  describe 'GET /:id' do
+    before do
+      ENV['FILEPATH'] = './quotes/example_quotes.json'
+    end
+
+    context 'with existing id' do
+      before do
+        @id = 'some_id_to_be_used_for_permalinking'
+      end
+
+      it 'returns a specific quote from file' do
+        get "/#{@id}"
+        expect(last_response.status).to eq 200
+        response = last_response.body
+        quote = JSON.parse(response)
+        expect(quote.class).to eq Hash
+        expect(quote.has_key?('who')).to eq true
+        expect(quote.has_key?('what')).to eq true
+        expect(quote.has_key?('where')).to eq true
+        expect(quote.has_key?('when')).to eq true
+        expect(quote.has_key?('id')).to eq true
+        expect(quote.has_key?('permalink')).to eq true
+        expect(quote['permalink'].include?(quote['id'])).to eq true
+        expect(quote['id']).to eq @id
+      end
+    end
+
+    context 'with non-existing id' do
+      before do
+        @id = 'some_gibberish'
+      end
+
+      it 'returns a 404' do
+        get "/#{@id}"
+        expect(last_response.status).to eq 500
+      end
+    end
+  end
+
+  describe 'get /html' do
+    before do
+      ENV['FILEPATH'] = './quotes/example_quotes.json'
+    end
+
+    it 'returns a html formatted quote' do
+      get '/html'
+      expect(last_response.status).to eq 200
+      quote = last_response.body
+      expect(quote.class).to eq String
+
+      expect(quote.include?('some_id_to_be_used_for_permalinking')).to eq true
+      expect(quote.include?('who said it')).to eq true
+      expect(quote.include?('what was said')).to eq true
+      expect(quote.include?('what show / movie / book')).to eq true
+      expect(quote.include?('which season / episode / chapter')).to eq true
+    end
+  end
+
+  describe 'get /:id/html' do
+    before do
+      ENV['FILEPATH'] = './quotes/example_quotes.json'
+    end
+
+    context 'with existing id' do
+      before do
+        @id = 'some_id_to_be_used_for_permalinking'
+      end
+
+      it 'returns a html formatted quote' do
+        get "/#{@id}/html"
+        expect(last_response.status).to eq 200
+        quote = last_response.body
+        expect(quote.class).to eq String
+
+        expect(quote.include?('some_id_to_be_used_for_permalinking')).to eq true
+        expect(quote.include?('who said it')).to eq true
+        expect(quote.include?('what was said')).to eq true
+        expect(quote.include?('what show / movie / book')).to eq true
+        expect(quote.include?('which season / episode / chapter')).to eq true
+      end
+    end
+
+    context 'with non-existing id' do
+      before do
+        @id = 'some_gibberish'
+      end
+
+      it 'returns a 404' do
+        get "/#{@id}/html"
+        expect(last_response.status).to eq 500
+      end
+    end
+  end
+end
